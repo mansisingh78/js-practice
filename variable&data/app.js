9808d4699e7e386d3bf218ac5f4d5ffd347916ec
Mansi singh <mansi123singh1234@gmail.com>
2024-08-22 09:12:25 +0530

    practice

--- variable&data/app.js
+++ variable&data/app.js
@@ -3,3 +3,33 @@ let age = 21;
 let x;
 let y = null;
 console.log(name);
+//practice concept
+
+const title = {
+  bag: "lethar",
+  price: 256,
+  rating: 5,
+  offer: 45,
+};
+//change object
+
+title["bag"] = "pencil";
+title["price"] = title["price"] + 10;
+console.log(title["bag"]);
+
+const product = {
+  name: "Ball pen",
+  price: 20,
+  rating: 4,
+};
+product["rating"] = 6;
+console.log(product["rating"]);
+
+const skills = {
+  js: 20,
+  css: 23,
+  html: 35,
+};
+
+skills["css"] = "react js";
+console.log(skills["css"]);
