18cd53e0bf357f145cd0c9928dedd5cd1d97d4e1
Mansi singh <mansi123singh1234@gmail.com>
2024-08-24 10:19:02 +0530

    new changing

---
+++ operator/script.js
@@ -0,0 +1,51 @@
+// Arithmetic operators
+
+// let a = 5;
+// let b = 2;
+// console.log("a + b = ", a + b);
+// console.log("a = ", a, "& b = ", b);
+// console.log("a - b = ", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b);
+
+// unary operator
+let a = 4;
+let b = 3;
+console.log("a = ", a, "& b = ", b);
+// a++;
+a--;
+console.log("a = ", a);
+
+// Assignment operator
+
+// let x = 5;
+// let y = 2;
+
+// a += 4; // a = a + 4
+// console.log("a = ", a);
+
+// comparision operator
+// let x = 5;
+// let y = 5;
+
+// console.log("x == y", x == y);
+// console.log("x != y", x != y);
+
+// equal to ==
+// not equal to !=
+// equal to & type ===
+// not equal to & type !==
+
+// let x = 5;
+// let y = 2;
+
+// console.log("x > y", x > y);
+// console.log("x >= y", x >= y);
+
+// Logical operators
+let x = 5;
+let y = 2;
+
+console.log("cond1 && cond2 = ", a > b || a === b);
